0488133972a9bddca7fb81f9c7642cd38870d7c4
Abhishek Kumar <abhi.kr.2100@gmail.com>
2023-09-16 13:08:56 +0530

    Add "what's" to apostrophe exceptions
    Signed-off-by: Abhishek Kumar <abhi.kr.2100@gmail.com>

--- consts.js
+++ consts.js
@@ -1,5 +1,5 @@
 // words with apostrophe that Duolingo doesn't separate
-const APS_EXCEPTIONS = ["don't", "i'm", "it's"];
+const APS_EXCEPTIONS = ["don't", "i'm", "it's", "what's"];
 
 // words whose punctuations shouldn't be deleted
 const PUNCT_EXCEPTIONS = ["Mr.", "Mrs."];
